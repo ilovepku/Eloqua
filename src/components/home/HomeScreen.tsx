879c90d07a2c9e2dac8b131c831d4ce58896c733
Sean Lee <seanleecoder@gmail.com>
2020-08-23 00:22:59 +0200

    + react-navigation, tailwind

---
+++ src/components/home/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import tailwind from 'tailwind-rn';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tailwind('h-full')}>
+      <View style={tailwind('pt-12 items-center')}>
+        <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+          <Text style={tailwind('text-blue-800 font-semibold')}>
+            Hello Tailwind
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
