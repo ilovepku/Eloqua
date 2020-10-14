506d662c1bbeacec6aba42546ebc6e2213f60ec8
Sean Lee <seanleecoder@gmail.com>
2020-10-14 14:52:20 +0200

    + CategoryItem: refactored from CategoriesScreen

--- src/components/categories/CategoriesScreen.tsx
+++ src/components/categories/CategoriesScreen.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import {View, TouchableOpacity, Image, Text} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {View} from 'react-native';
 import {useQuery} from '@apollo/client';
 import tailwind from 'tailwind-rn';
 
 import categoriesQuery from '../../graphql/query/categoriesQuery';
 import {CategoriesQuery_category} from '../../types/graphql';
-import {ASSETS_URL} from '../../settings';
+
 import Error from '../error/Error';
 import LoadingIndicator from '../loadingIndicator/LoadingIndicator';
+import CategoryItem from './CategoryItem';
 
 export default function CategoriesScreen() {
-  const navigation = useNavigation();
   const {data, loading, error} = useQuery(categoriesQuery);
 
   return error ? (
@@ -20,29 +19,9 @@ export default function CategoriesScreen() {
     <LoadingIndicator />
   ) : (
     <View style={tailwind('flex-1 bg-white flex-row flex-wrap justify-evenly')}>
-      {(data?.categories ?? []).map(
-        ({id, name, icon_filename}: CategoriesQuery_category) => (
-          <TouchableOpacity
-            key={`category-${id}`}
-            style={tailwind('w-1/3 h-24 mt-4 items-center')}
-            onPress={() =>
-              navigation.navigate('CategoryPiecesList', {
-                title: name,
-                category_id_filter: `category-${id}`,
-              })
-            }>
-            <Image
-              source={{
-                uri: `${ASSETS_URL}/icons%2F${icon_filename}?alt=media`,
-              }}
-              style={tailwind('h-20 w-20 rounded-full')}
-            />
-            <View style={tailwind('flex-1 text-gray-700')}>
-              <Text style={tailwind('text-center text-xs')}>{name}</Text>
-            </View>
-          </TouchableOpacity>
-        ),
-      )}
+      {(data?.categories ?? []).map((category: CategoriesQuery_category) => (
+        <CategoryItem key={`category-${category.id}`} category={category} />
+      ))}
     </View>
   );
 }
---
+++ src/components/categories/CategoryItem.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, TouchableOpacity, Image, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import tailwind from 'tailwind-rn';
+
+import {CategoriesQuery_category} from '../../types/graphql';
+import {ASSETS_URL} from '../../settings';
+
+interface Props {
+  category: CategoriesQuery_category;
+}
+
+export default function CategoryItem({
+  category: {name, id, icon_filename},
+}: Props) {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      style={tailwind('w-1/3 h-24 mt-4 items-center')}
+      onPress={() =>
+        navigation.navigate('CategoryPiecesList', {
+          title: name,
+          category_id_filter: `category-${id}`,
+        })
+      }>
+      <Image
+        source={{
+          uri: `${ASSETS_URL}/icons%2F${icon_filename}?alt=media`,
+        }}
+        style={tailwind('h-20 w-20 rounded-full')}
+      />
+      <View style={tailwind('flex-1 text-gray-700')}>
+        <Text style={tailwind('text-center text-xs')}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
