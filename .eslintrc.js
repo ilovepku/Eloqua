a7a7cc022782ed5a3245c11bc841456e0d640709
Sean Lee <seanleecoder@gmail.com>
2020-11-07 10:40:46 +0100

    * update packages, jest & lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'plugin:jest/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint'],
 };
